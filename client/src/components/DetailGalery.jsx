018b16c763e262c42e1ca40fa61ce91be86ce15e
Titus Herdiawan <titus.herdiawan@gmail.com>
2020-08-24 17:24:10 +0700

    titus

--- client/src/components/DetailGalery.jsx
+++ client/src/components/DetailGalery.jsx
@@ -1,47 +1,96 @@
 import React, { useEffect, useState } from "react";
-import logoipebi from '../assets/logoipebi.png'
-import logohut from '../assets/logohut.png'
-import logori from '../assets/logori.png'
+import logoipebi from "../assets/logoipebi.png";
+import logohut from "../assets/logohut.png";
+import logori from "../assets/logori.png";
 // import painting from '../assets/painting.jpg'
 
 function Detail(props) {
-    useEffect(() => { }, []);
-    console.log(props);
-    return (
-        <div className="container-fluid pl-4 pt-4" style={{ position: "fixed", backgroundColor: "white", zIndex: "999", height: "100%" }}>
-            <div className="row">
-                <div className="col-1" style={{ minWidth: "15vh" }}>
-                    <img src={logoipebi} className="img-fluid w-10 float-left" alt="logoipebi" />
-                </div>
-                <div className="col-1" style={{ minWidth: "15vh" }}>
-                    <img src={logohut} className="img-fluid w-10 float-left" alt="logohut" />
-                </div>
-                <div className="col-1" style={{ minWidth: "20vh" }}>
-                    <img src={logori} className="img-fluid w-10 float-left" alt="logori" />
-                </div>
-            </div>
-            <div className="row p-5">
-                <div className="col" style={{ minWidth: "78vh" }}>
-                    <img src={props.dataDetail.image} className="img-fluid float-left" width="550" height="1000" alt="painting" />
-                </div>
-                <div className="col px-3" style={{ minWidth: "72vh", marginLeft: "1.5vh" }}>
-                    <div className="row"><h2>{props.dataDetail.nama}</h2></div>
-                    <div className="row"><p className="m-0 mt-4 text-justify">bidang: {props.dataDetail.bidang}</p></div>
-                    <div className="row"><p className="m-0 text-justify">juara: 👑</p></div>
-                    <div className="row"><p className="m-0 mb-4 text-justify">medali: 🎉🎉🎉</p></div>
-                    <div className="row">
-                        <p className="text-justify">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam repellendus iusto nam ullam optio aspernatur quam id, quasi neque! Deleniti asperiores tenetur laudantium magni blanditiis ipsam aliquam ducimus molestias sunt.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam repellendus iusto nam ullam optio aspernatur quam id, quasi neque! Deleniti asperiores tenetur laudantium magni blanditiis ipsam aliquam ducimus molestias sunt.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="row fixed-bottom" style={{ left: "5vh" }}>
-                <p style={{ cursor: "pointer" }} onClick={props.handleCloseDetail}>⬅️ Kiri</p>
-            </div>
+  useEffect(() => {}, []);
+  console.log(props);
+  return (
+    <div
+      className="container-fluid pl-4 pt-4"
+      style={{
+        position: "fixed",
+        backgroundColor: "#c8c4c4",
+        zIndex: "999",
+        height: "100%",
+      }}
+    >
+      <div className="row">
+        <div className="col-1" style={{ minWidth: "15vh" }}>
+          <img
+            src={logoipebi}
+            className="img-fluid w-10 float-left"
+            alt="logoipebi"
+          />
         </div>
-    );
+        <div className="col-1" style={{ minWidth: "15vh" }}>
+          <img
+            src={logohut}
+            className="img-fluid w-10 float-left"
+            alt="logohut"
+          />
+        </div>
+        <div className="col-1" style={{ minWidth: "20vh" }}>
+          <img
+            src={logori}
+            className="img-fluid w-10 float-left"
+            alt="logori"
+          />
+        </div>
+      </div>
+      <div className="row p-5">
+        <div className="col" style={{ minWidth: "78vh" }}>
+          <img
+            src={props.dataDetail.image}
+            className="img-fluid float-left"
+            width="550"
+            height="1000"
+            alt="painting"
+          />
+        </div>
+        <div
+          className="col px-3"
+          style={{ minWidth: "72vh", marginLeft: "1.5vh" }}
+        >
+          <div className="row">
+            <h2><b>{props.dataDetail.nama}</b></h2>
+          </div>
+            <p>{props}</p>
+          <div className="row">
+            <p className="m-0 mt-4 text-justify">
+              <b> Bidang </b>    : {props.dataDetail.bidang}
+            </p>
+          </div>
+          <div className="row">
+            <p className="m-0 text-justify"><b> Juara </b>   : 1</p>
+          </div>
+          <div className="row">
+            <p className="m-0 mb-4 text-justify"><b> Medali </b> : Emas</p>
+          </div>
+          <div className="row">
+            <p className="text-justify">
+              Virtual Photoshoot merupakan suatu inovasi di bidang fotografi
+              dengan cara melakukan pengambilan hasil karya foto melalui video
+              conference. Lomba Virtual Photoshoot Digifestabi 2020 memberi
+              pengalaman baru bagi talenta-talenta fotografi Bank Indonesia.
+              Alih-alih sesi foto secara live, para fotografer membidikkan
+              kameranya ke laptop dan mengarahkan model yang berada di sisi lain
+              layar. Mengambil tema <b> Generasi Muda Indonesia sebagai Inspirasi
+              Transformasi Digital” </b>, para fotografer BI telah mampu menunjukkan
+              kebolehannya dengan menghasilkan foto-foto berkualitas.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row fixed-bottom" style={{ left: "5vh" }}>
+        <p style={{ cursor: "pointer" }} onClick={props.handleCloseDetail}>
+          ⬅️ Ruang Seni IPEBI
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import painting from '../assets/painting.jpg'
-import DetailGalery from '../components/DetailGalery'
+import one from '../assets/painting.jpeg';
+import painting1 from '../assets/painting.jpg'
+import DetailGalery from '../components/DetailGalery';
+import XDVideo from '../assets/hutrivideo.mp4';
 
 function Homepage(props) {
     useEffect(() => { }, []);
@@ -9,14 +11,14 @@ function Homepage(props) {
     const [dataDetail] = useState(
         [
             {
-                image: painting,
-                nama: "Ahmad Zuliansyah Putra",
-                bidang: "green"
+                image: one,
+                nama: "Afriezal Raeshandi",
+                bidang: "Merah"
             },
             {
-                image: painting,
-                nama: "Ahmad Zuliansyah",
-                bidang: "blue"
+                image: painting1,
+                nama: "Gde Agus Dwijaya K.",
+                bidang: "Biru"
             }
         ]
     )
@@ -30,37 +32,7 @@ function Homepage(props) {
     return (
         <div class="gallery">
             {/* dari sini */}
-            <div className="container-fluid pl-5 pt-5 pr-5" style={{ zIndex: "99", display: "inline-block", position: "absolute", top: "5vh" }}>
-                <div className="row text-center">
-                    <h1 className="font-weight-bold w-100 m-0">RUANG SENI</h1>
-                </div>
-                <div className="row text-center" style={{ marginTop: "-4vh" }}>
-                    <h1 className="font-weight-bold w-100 m-0 display-1">IPEBI</h1>
-                </div>
-                <div className="row d-flex justify-content-center pl-5 pr-5 pt-3">
-                    <img src={painting} className="img-fluid float-left" width="300" height="1000" alt="painting" />
-                </div>
-                <div className="row text-center mt-3">
-                    <h5 className="font-weight-bold w-100 m-0">about</h5>
-                </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-start" style={{ minWidth: "26vh", maxWidth: "26vh", left: "0", position: "absolute" }}>
-                        <h2 className="text-right font-weight-bold">Virtual Photoshoot</h2>
-                    </div>
-                    <div className="col d-flex justify-content-end" style={{ minWidth: "26vh", right: "0", maxWidth: "26vh", position: "absolute" }}>
-                        <h2 className="text-left font-weight-bold">Live Painting</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="gallery__group">
-                {
-                    dataDetail.map((item, index) => {
-                        return (
-                            <img key={index} onClick={() => handleShowDetail(index)} width="500" height="500" className="img_hov gallery__img" src={item.image} alt="" />
-                        )
-                    })
-                }
-            </div>
+
             {/* sampe sini */}
 
             {
@@ -69,6 +41,44 @@ function Homepage(props) {
                     :
                     <div></div>
             }
+
+             {/* dari sini */}
+             
+             <div className="container-fluid pl-5 pt-5 pr-5" style={{ zIndex: "99", display: "inline-block", position: "absolute", width:"100", top: "-4vh", fontFamily: 'Alata'}}>
+                <div className="row text-center">
+                    <h1 className="font-weight-bold w-100 m-0">About</h1>
+                </div>
+                <div className="row d-flex justify-content-center pl-5 pr-5 pt-3">
+                    <video src={XDVideo} className="img-fluid float-left" width="430" height="1000" alt="painting" autoplay="true"/>
+                {/* <div className="col d-flex justify-content-start" style={{ minWidth: "26vh", maxWidth: "26vh", left: "19vh", position: "absolute", top: "35vh" }}>
+                    <h2 className="text-right font-weight-bold">Virtual Photoshoot</h2>
+                </div>
+                <div className="col d-flex justify-content-end" style={{ minWidth: "26vh", right: "24vh", maxWidth: "26vh", position: "absolute", top: "35vh" }}>
+                    <h2 className="text-left font-weight-bold">Live Painting</h2>
+                </div> */}
+            {/* </div> */}
+                </div>
+                    <div className="row text-center">
+                    <h1 className="font-weight-bold w-100 m-0">RUANG SENI</h1>
+                </div> 
+                    <div className="row text-center" style={{ marginTop: "-4vh"}}>
+                    <h1 className="font-weight-bold w-100 m-0 display-1">IPEBI</h1>
+                </div> 
+                {/* <div className="row text-center mt-3">
+                    <h5 className="font-weight-bold w-100 m-0">about</h5>
+                </div> */}
+                {/* <div className="row"> */}
+            </div>
+            <div class="gallery__group">
+                {
+                    dataDetail.map((item, index) => {
+                        return (
+                            <img key={index} onClick={() => handleShowDetail(index)} width="350" height="250" className="img_hov gallery__img" src={item.image} alt="" />
+                        )
+                    })
+                }
+            </div>
+            {/* sampe sini */}
 
             {/* taro di sini */}
         </div>
